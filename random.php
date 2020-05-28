2c71d485dedc84087bb054177736a00122016d25
nagyrobi <robreg@zsurob.hu>
2020-05-28 11:19:58 +0200

    serve pictures separately based on client IP

--- random.php
+++ random.php
@@ -1,19 +1,48 @@
 <?php
 
-$root = '';
+$root = '/var/www/html/utils/random_img/';
+//$root = '';
 // use if specifying path from root
 //$root = $_SERVER['DOCUMENT_ROOT'];
 
-
-// the directory containing the JPG files
-//$path = 'images/';
-$path = '/var/www/html/utils/random_img/images/';
-
-// a temporary logfile to remember the last pictures thrown
-$prev = '/tmp/piclog.txt';
-
 // this many pictures to remember no to show soon
-$noRepeatLast = 60;
+$noRepeatLast = 30;
+
+
+$clientip = getUserIpAddr();
+
+switch ($clientip) {
+    case "192.168.1.11":
+        $path = $root . 'images-1/'; 			// the directory containing the JPG files
+    	$prev = '/tmp/piclog-display-1.txt';	// a temporary logfile to remember the last pictures thrown
+        break;
+    case "192.168.1.12":
+        $path = $root . 'images-2/';
+	    $prev = '/tmp/piclog-display-2.txt';
+        break;
+    case "192.168.1.13":
+        $path = $root . 'images/';
+        $prev = '/tmp/piclog-display-3.txt';
+        break;
+    default:
+        $path = $root . 'images/';
+    	$prev = '/tmp/piclog-others.txt';
+        break;
+}
+
+
+function getUserIpAddr(){
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+        //ip from share internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        //ip pass from proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
 
 function getImagesFromDir($path) {
     $images = array();
@@ -36,17 +65,17 @@ function getRandomFromArray($ar) {
 
 
 // Obtain list of images from directory
-$imgList = getImagesFromDir($root . $path);
+$imgList = getImagesFromDir($path);
 
 // Fill the temporary logfile with some data
 if (file_exists($prev)) {
-        $previmgss = file_get_contents($prev);
+	$previmgss = file_get_contents($prev);
 } else {
-        for($i = 0; $i < $noRepeatLast; ++$i) {
-                $img = getRandomFromArray($imgList);
-                file_put_contents($prev, "|" . $img, FILE_APPEND);;
-        }
-        $previmgss = file_get_contents($prev);
+	for($i = 0; $i < $noRepeatLast; ++$i) {
+		$img = getRandomFromArray($imgList);
+		file_put_contents($prev, "|" . $img, FILE_APPEND);;
+	}
+	$previmgss = file_get_contents($prev);
 }
 
 // Get the list of the last pictures displayed so far
@@ -63,11 +92,14 @@ $img = getRandomFromArray($imgListClean);
 file_put_contents($prev, $img . "|");
 file_put_contents($prev, implode("|", $previmgs), FILE_APPEND);
 
+//date_default_timezone_set("Europe/Amsterdam");
+//file_put_contents('/tmp/piclogtime.txt', date("Y-m-d H:i:s") . " : " . getUserIpAddr() . " : " . $img . "\n", FILE_APPEND);
+
 //print_r ($img);
+//echo "\n";
 //print_r ($previmgs);
 //print_r ($imgListClean);
 //print_r ($imgList);
-
 
 // Serve out the picture as JPG
 header("Content-type: image/jpg");
